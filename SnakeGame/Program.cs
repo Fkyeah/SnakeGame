1d5c55841e2f9bbeb7033762b7560af8383249d3
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2022-02-06 13:53:55 +0300

    Рефакторинг игрового процесса (#2)

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -9,26 +9,43 @@ namespace SnakeGame
 {
     class Program
     {
+        private static int _windowHeigth = 28;
+        private static int _windowWidth = 102;
+        private static int _gameMapHeigth = _windowHeigth - 3;
+        private static int _gameMapWidth = _windowWidth - 2;
+        private static int _startPositionX = 5;
+        private static int _startPositionY = 4;
+        private static Direction _startDirection = Direction.RIGHT;
+        private static char _eatSymbol = '$';
+        private static char _snakeSymbol = '*';
+        private static int _snakeLength = 5;
+
         static void Main(string[] args)
         {
             int Score = 0;
-            Console.SetWindowSize(102, 28);
-            Console.SetBufferSize(102, 28);
+            Console.SetWindowSize(_windowWidth, _windowHeigth);
+            Console.SetBufferSize(_windowWidth, _windowHeigth);
             Console.CursorVisible = false;
-            Walls walls = new Walls(102, 27);
+
+            Walls walls = new(_windowWidth, _windowHeigth - 1);
             walls.Draw();
-            GameMenu GameParams = new GameMenu();
-            Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 5, Direction.RIGHT);
-            FoodCreator foodCreator = new FoodCreator(100, 25, '$');
+
+            GameMenu GameParams = new();
+
+            Point p = new(_startPositionX, _startPositionY, _snakeSymbol);
+            Snake snake = new(p, _snakeLength, _startDirection);
+
+            FoodCreator foodCreator = new(_gameMapWidth, _gameMapHeigth, _eatSymbol);
             Point food = foodCreator.CreateFood();
-            Point food2 = foodCreator.CreateFood();
+
             snake.Draw();
             food.Draw();
+
             while (true)
             {
                 GameParams.WriteMenu();
                 GameParams.WriteScore(Score);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
@@ -54,6 +71,7 @@ namespace SnakeGame
                 }
                 else
                     Thread.Sleep(100);
+
                 snake.Move();
             }
 
