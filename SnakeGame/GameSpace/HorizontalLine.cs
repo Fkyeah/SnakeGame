dd409b449d606d70a6ca4ebbc8a3593931089ff0
Fkyeah <dimx63@mail.ru>
2022-02-06 13:35:57 +0300

    Рисуем игровую область

---
+++ SnakeGame/GameSpace/HorizontalLine.cs
@@ -0,0 +1,18 @@
+﻿using SnakeGame.Base;
+using System.Collections.Generic;
+
+namespace SnakeGame.GameSpace
+{
+    public class HorizontalLine : Figure
+    {
+        public HorizontalLine(int x, int yUp, int yDown, char symbol)
+        {
+            _pointList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new(x, y, symbol);
+                _pointList.Add(p);
+            }
+        }
+    }
+}
